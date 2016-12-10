16beba4c4dc38f1051499988efc0dd826667121b
Perry Miller <perry@modelspace.com>
2016-12-10 15:53:27 -0500

    -- Adding grunt config file. -- Adding npm package file. -- Adding source file. -- Adding class file.

---
+++ gruntfile.js
@@ -0,0 +1,75 @@
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2016, Perry L Miller IV
+//  All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Grunt configuration file.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+/* eslint-env node */
+
+module.exports = function ( grunt )
+{
+  // Initialize all the scripts we can run.
+  var scripts = require ( "./package.json" ).scripts;
+  var commands = {};
+  for ( var key in scripts )
+  {
+    commands[key] = { cmd: "npm", args: [ "run", key ] };
+  }
+
+  // All configuration goes here
+  var config = {
+
+    // The arbitrary commands to run, defined above.
+    run: commands,
+
+    // Configure what files to watch, and what to do when they change.
+    watch: {
+
+      // The scripts are a combination of the files to watch and the
+      // comands to run when they change.
+      scripts: {
+
+        // The files to watch.
+        files: [
+
+          // Configuration files.
+          "gruntfile.js",
+          "package.json",
+
+          // Classes, templates, styles, etc.
+          "latex/**/*.cls",
+
+          // Source of the paper.
+          "source/**/*.tex"
+        ],
+
+        // Do these tasks when a file changes.
+        tasks: [
+          "run:clear",
+          "run:build"
+        ],
+
+        // Do not spawn a child process to run the tasks.
+        options: { spawn: false }
+      }
+    }
+  };
+
+  // All configuration goes here
+  grunt.initConfig ( config );
+
+  // Tell grunt we plan to use these plug-ins.
+  grunt.loadNpmTasks ( "grunt-run" );
+  grunt.loadNpmTasks ( "grunt-contrib-watch" );
+
+  // What grunt should do when we type "grunt" into the terminal.
+  grunt.registerTask ( "default", [ "run:build" ] );
+};
